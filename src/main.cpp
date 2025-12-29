73be97f2a2d1b18dc6958531445b04ac20752c04
Sam-Desktop <samueldavidanderson2001@gmail.com>
2025-12-28 19:51:54 -0800

    game update 0.1

--- src/main.cpp
+++ src/main.cpp
@@ -13,40 +13,62 @@ by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit h
 
 int main ()
 {
+	// Initialization
+	//--------------------------------------------------------------------------------------
 	// Tell the window to use vsync and work on high DPI displays
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
+	//initialize variables
+	int screenWidth = 1280;
+	int screenHeight = 800;
+	Vector2 playerPosition = { (float)screenWidth / 2, (float)screenHeight / 2 };
+	Vector2 zombiePosition = {20,20};
 	// Create the window and OpenGL context
-	InitWindow(1280, 800, "Hello Raylib");
+	InitWindow(screenWidth, screenHeight, "Window 1");
+
+	//fps target
+	SetTargetFPS(60);
+
 
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
 
 	// Load a texture from the resources directory
-	Texture wabbit = LoadTexture("wabbit_alpha.png");
-	
+	//Texture wabbit = LoadTexture("wabbit_alpha.png");
+	//--------------------------------------------------------------------------------------
+
+
 	// game loop
 	while (!WindowShouldClose())		// run the loop until the user presses ESCAPE or presses the Close button on the window
 	{
-		// drawing
+		// Update
+		//----------------------------------------------------------------------------------
+		if (IsKeyDown(KEY_D)) playerPosition.x += 2.0f;
+		if (IsKeyDown(KEY_A)) playerPosition.x -= 2.0f;
+		if (IsKeyDown(KEY_W)) playerPosition.y -= 2.0f;
+		if (IsKeyDown(KEY_S)) playerPosition.y += 2.0f;
+		// get zombie to follow player 
+		//----------------------------------------------------------------------------------
+		if (zombiePosition.x != playerPosition.x) {
+
+
+		}
+		if (zombiePosition.y != playerPosition.y) {
+
+		}
+		// Draw
+		//----------------------------------------------------------------------------------
 		BeginDrawing();
 
-		// Setup the back buffer for drawing (clear color and depth buffers)
-		ClearBackground(BLACK);
+			ClearBackground(BROWN);
+			DrawCircleV(playerPosition, 15.0f, SKYBLUE);
+			DrawCircleV(zombiePosition, 15.0f, RED);
 
-		// draw some text using the default font
-		DrawText("Hello Raylib", 200,200,20,WHITE);
-
-		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
-		
-		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
 	}
 
 	// cleanup
 	// unload our texture so it can be cleaned up
-	UnloadTexture(wabbit);
 
 	// destroy the window and cleanup the OpenGL context
 	CloseWindow();
